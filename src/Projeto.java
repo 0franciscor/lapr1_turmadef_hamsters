1d4916d5bf9f15f96d251673898f952141bd494a
RitaAriana <1201386@isep.ipp.pt>
2021-01-08 14:46:19 +0000

    otimização de alguns métodos - criação do método para imprimir informação

--- src/Projeto.java
+++ src/Projeto.java
@@ -7,11 +7,12 @@ public class Projeto {
     static Scanner ler = new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException {
-        double populacaoInicial[] = tratamentoDados((leituraDados("Hamsters.txt", 0)));     //VETOR INICIAL
-        double matrizLeslie[][] = new double [populacaoInicial.length][populacaoInicial.length];                           //DECLARAÇÃO MATRIZ LESLIE
+        double [] populacaoInicial = tratamentoDados((leituraDados("Hamsters.txt", 0)));     //VETOR INICIAL
+        double [][] matrizLeslie = new double [populacaoInicial.length][populacaoInicial.length];                           //DECLARAÇÃO MATRIZ LESLIE
         for(int i = 1; i<=2; i++){
             insercaoMatriz(matrizLeslie, leituraDados("Hamsters.txt", i), i);
         }
+        int n=matrizLeslie.length;
 
         System.out.println("Quais as geracoes que pretende que sejam estudadas?");
         System.out.println("Para terminar a introducao das geracoes a analisar digite -1");
@@ -23,35 +24,17 @@ public class Projeto {
         double [][] Nt = new double[1000][matrizLeslie.length];
         double [][] distribuicaoNormalizada = new double[1000][matrizLeslie.length];
 
-
         while (t!=-1){
             geracao+=1;
             geracoesEstimadas [geracao]= t;
-            dimensaoPopulacao(matrizLeslie,populacaoInicial,t,Nt,populacoesEstimadas,geracao,distribuicaoNormalizada);
-            dimensaoPopulacao(matrizLeslie,populacaoInicial,(t+1),Nt,populacoesEstimadas,(geracao+1),distribuicaoNormalizada);
+            dimensaoPopulacao(matrizLeslie,populacaoInicial,t,Nt,populacoesEstimadas,geracao);
+            dimensaoPopulacao(matrizLeslie,populacaoInicial,(t+1),Nt,populacoesEstimadas,(geracao+1));
             TaxaVariacao(populacoesEstimadas,geracao,taxasDeVariacao);
+            distribuicaoNormalizada(geracao,Nt,populacoesEstimadas,distribuicaoNormalizada,n);
             t=ler.nextInt();
         }
 
-        // colocar esta informação a ser imprimida num método
-        for (int l=0; l<=geracao;l++){
-            System.out.print("A populacao total na geracao " + geracoesEstimadas[l] +" e ");
-            System.out.printf("%.2f\n", populacoesEstimadas[l]);
-            System.out.print("Sendo a taxa de variacao nesta geracao de ");
-            System.out.printf("%.2f\n", taxasDeVariacao[l]);
-            System.out.print("A distribuicao da populacao e a seguinte: ");
-            for (int j=0; j< matrizLeslie.length;j++){
-                System.out.printf("%.2f ", Nt[l][j]);
-            }
-            System.out.println("");
-            System.out.print("A distribuicao normalizada da populacao e a seguinte: ");
-            for (int j=0; j< matrizLeslie.length;j++){
-                System.out.printf("%.3f ", distribuicaoNormalizada[l][j]);
-            }
-            System.out.println("");
-            System.out.println("");
-
-        }
+        imprimirAnalisegeracoes(geracao,geracoesEstimadas,populacoesEstimadas,taxasDeVariacao,distribuicaoNormalizada,Nt,n);
     }
 
     public static String leituraDados(String nomeFicheiro, int numLinha) throws FileNotFoundException { //LEITURA EXCLUSIVA DO VETOR
@@ -61,14 +44,12 @@ public class Projeto {
         for(int i = 0; i<=numLinha; i++)
             input = leituraFicheiro.nextLine();
         leituraFicheiro.close();
-
         return input;
     }
 
     public static double[] tratamentoDados(String input) {
         String[] dadosInseridos = input.split(", ");
         double [] valoresTratados = new double[dadosInseridos.length];
-
         for(int i = 0; i< dadosInseridos.length; i++) {
             String[] dadosInseridosTratados = dadosInseridos[i].split("=");
             double valorTratado = Double.parseDouble(dadosInseridosTratados[1]);
@@ -79,7 +60,6 @@ public class Projeto {
 
     public static double[][] insercaoMatriz (double[][] matrizLeslie, String input, int tipologia) throws FileNotFoundException { //LEITURA E INSERÇÃO DE DADOS NA MATRIZ DE LESLIE 0-SOBREVIVENCIA 1- FECUNDIDADE
         double[] valoresTratados = tratamentoDados(input);
-
         if (tipologia == 1) {
             int colunaCorrente = 0;
             for (int i = 1; i <= valoresTratados.length; i++) {
@@ -94,36 +74,35 @@ public class Projeto {
         return matrizLeslie;
     }
 
-    public static void dimensaoPopulacao (double [][] matrizLeslie, double[] populacaoInicial, int t, double[][] Nt,double[] populacoesEstimadas, int geracao, double[][] distribuicaoNormalizada) throws FileNotFoundException{ //CALCULO DIMENSAO POPULACAO
-        // tentar criar esta nova matriz de outra maneira
-        double Lesliemultiplicada[][] = new double[matrizLeslie.length][matrizLeslie.length];
+    public static void dimensaoPopulacao (double [][] matrizLeslie, double[] populacaoInicial, int t, double[][] Nt,double[] populacoesEstimadas, int geracao) throws FileNotFoundException{ //CALCULO DIMENSAO POPULACAO
+        double [][] Lesliemultiplicada = new double[matrizLeslie.length][matrizLeslie.length];
         for(int i =0 ; i<matrizLeslie.length; i++){
             for(int j =0 ; j< matrizLeslie.length; j++){
                 Lesliemultiplicada[i][j]=matrizLeslie[i][j];
             }
         }
-        int g=2;
-        while (g<=t){
+        for(int temp = t; temp>=2;temp--){
             multiplicarmatrizes(matrizLeslie,Lesliemultiplicada);
-            g+=1;
         }
 
         double total=0;
-        int i,l;
-        for (i=0;i<matrizLeslie.length;i++){
-            double somas=0;
-            for (l=0;l< matrizLeslie.length;l++){
-                somas=somas+Lesliemultiplicada[i][l]*populacaoInicial[l];
+        if (t==0){
+            for (int a=0;a<matrizLeslie.length;a++){
+                Nt[geracao][a]=populacaoInicial[a];
             }
-            Nt[geracao][i]=somas;
+        } else {
+            for (int i=0;i<matrizLeslie.length;i++){
+                double somas=0;
+                for (int l=0;l< matrizLeslie.length;l++){
+                    somas=somas+Lesliemultiplicada[i][l]*populacaoInicial[l];
+                }
+                Nt[geracao][i]=somas;
+            }
         }
-        for (i=0;i< matrizLeslie.length;i++){
-            total=total+Nt[geracao][i];
+        for (int i=0;i< matrizLeslie.length;i++){
+            total+=Nt[geracao][i];
         }
         populacoesEstimadas[geracao]=total;
-        for (i=0;i< matrizLeslie.length;i++){
-            distribuicaoNormalizada[geracao][i]=Nt[geracao][i]/populacoesEstimadas[geracao];
-        }
     }
 
     public static void TaxaVariacao(double [] populacoesEstimadas, int geracao, double[] taxasDeVariacao) {
@@ -146,6 +125,31 @@ public class Projeto {
             for (c=0;c<matriz2.length;c++){
                 matriz2[l][c]=aux[c];
             }
+        }
+    }
+    public static void distribuicaoNormalizada (int geracao,double[][] Nt, double [] populacoesEstimadas, double[][] distribuicaoNormalizada, int n){
+        for (int i=0;i<n;i++){
+            distribuicaoNormalizada[geracao][i]=Nt[geracao][i]/populacoesEstimadas[geracao];
+        }
+    }
+
+    public static void imprimirAnalisegeracoes (int geracao,int [] geracoesEstimadas,double[] populacoesEstimadas,double[] taxasDeVariacao,double [][] distribuicaoNormalizada, double[][] Nt, int n){
+        for (int l=0; l<=geracao;l++){
+            System.out.print("A populacao total na geracao " + geracoesEstimadas[l] +" e ");
+            System.out.printf("%.15f\n", populacoesEstimadas[l]);
+            System.out.print("Sendo a taxa de variacao nesta geracao de ");
+            System.out.printf("%.4f\n", taxasDeVariacao[l]);
+            System.out.print("A distribuicao da populacao e a seguinte: ");
+            for (int j=0; j<n;j++){
+                System.out.printf("%.2f ", Nt[l][j]);
+            }
+            System.out.println("");
+            System.out.print("A distribuicao normalizada da populacao e a seguinte: ");
+            for (int j=0; j<n;j++){
+                System.out.printf("%.3f ", distribuicaoNormalizada[l][j]);
+            }
+            System.out.println("");
+            System.out.println("");
         }
     }
 }
