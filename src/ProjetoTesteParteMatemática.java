318156114da2623868704da51fad88ce613ded04
RitaAriana <1201386@isep.ipp.pt>
2021-01-17 15:34:37 +0000

    no message

--- src/ProjetoTesteParteMatemática.java
+++ src/ProjetoTesteParteMatemática.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjetoTesteParteMatemática {
-
     @Test
     void valorModulo_numeroNegativo() {
         double resultado=Projeto.valorModulo(-1.5);
@@ -14,14 +12,12 @@ class ProjetoTesteParteMatemática {
         double resultado=Projeto.valorModulo(1.5);
         assertEquals((1.5),resultado);
     }
-
     @Test
     void totalPopulacao() {
         double[] resultado = new double[1];
         Projeto.totalPopulacao(0,resultado,new double[][]{{1000,300,330,100}});
         assertEquals(1730,resultado[0]);
     }
-
     @Test
     void copiarMatriz() {
         double [][] matriz=new double[2][2];
@@ -30,14 +26,12 @@ class ProjetoTesteParteMatemática {
         Projeto.copiarMatriz(matriz,resultado);
         assertArrayEquals(matriz,resultado);
     }
-
     @Test
     void taxaVariacao() {
         double[] resultado=new double[1];
         Projeto.taxaVariacao(new double[]{1000,1500},0,resultado);
         assertEquals(1.5,resultado[0]);
     }
-
     @Test
     void multiplicarMatrizesQuadradas() {
         double [][] matriz2=new double[2][2];
@@ -45,20 +39,17 @@ class ProjetoTesteParteMatemática {
         Projeto.multiplicarMatrizesQuadradas(new double[][]{{1,2},{3,2}},matriz2);
         assertArrayEquals(new double[][]{{7,6},{9,10}},matriz2);
     }
-
     @Test
     void distribuicaoNormalizada() {
         double[][]resultado=new double[1][4];
         Projeto.distribuicaoNormalizada(0,new double[][]{{500,200,200,100}},new double[]{1000},resultado);
         assertArrayEquals(new double[][]{{50,20,20,10}},resultado);
     }
-
     @Test
     void calcularMaiorValorProprio() {
         int coluna=Projeto.calcularMaiorValorProprio(new double[][]{{4,0,0},{0,3,0},{0,0,1}});
         assertEquals(0,coluna);
     }
-
     @Test
     void normalizarVetorProprio() {
         double [] vetor=new double[3];
@@ -66,7 +57,6 @@ class ProjetoTesteParteMatemática {
         Projeto.normalizarVetorProprio(vetor);
         assertArrayEquals(new double[]{45,30,25},vetor);
     }
-
     @Test
     void distribuicaoPopulacao() {
         double [][] distribuicao=new double[3][3];
@@ -74,7 +64,6 @@ class ProjetoTesteParteMatemática {
         assertArrayEquals(new double[][]{{0,0,0},{0,0,0},{100,100,25}},distribuicao);
 
     }
-
     @Test
     void calcularVetorValorProprio() {
         double maior=Projeto.calcularVetorValorProprio(new double[][]{{1,0,0},{0.5,1,0},{-1,2,2}},new double[3]);
